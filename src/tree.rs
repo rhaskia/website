9cd79d3e725e91d1c6de90577c0b9127adfb9287
rhaskia <rhaskiagamedev@gmail.com>
2024-04-20 09:57:19 +1200

    working tree

--- src/tree.rs
+++ src/tree.rs
@@ -6,6 +6,10 @@ use dioxus::router::components::IntoRoutable;
 #[component]
 pub fn Tree() -> Element {
     let blogs = use_resource(get_blogs);
+    let projects = BlogFile::Folder {
+        path: "projects".to_string(),
+        children: vec![BlogFile::File { path: String::from("PreTTY") }],
+    };
 
     rsx! {
         pre {
@@ -14,22 +18,45 @@ pub fn Tree() -> Element {
             r#"~/website"#
 
             if let Some(Ok(blog)) = &*blogs.read_unchecked() {
-                TreeFolder { lead: "assets/blog/", file: blog.clone(), rec: 0, indent: "", icon: "" }
+                TreeFolder { 
+                    lead: "/blog/",
+                    file: blog.clone(),
+                    rec: 0,
+                    indent: "",
+                    old_indent: "",
+                    icon: ""
+                }
+            }
+            TreeFolder {
+                lead: "/projects/",
+                file: projects,
+                rec: 0,
+                indent: "",
+                old_indent: "",
+                icon: ""
+
             }
         }
     }
 }
 
 #[component]
-pub fn TreeFolder(lead: String, file: BlogFile, rec: usize, indent: String, icon: String) -> Element {
+pub fn TreeFolder(
+    lead: String,
+    file: BlogFile,
+    rec: usize,
+    indent: String,
+    old_indent: String,
+    icon: String,
+) -> Element {
     let mut open = use_signal(|| false);
 
     rsx! {
         match file {
-            BlogFile::Folder { ref path, ref children } => rsx! { 
+            BlogFile::Folder { ref path, ref children } => rsx! {
                 div {
                     onclick: move |_| open.set(!open()),
-                    {format!("{indent}{}", if open() { " " } else { " " })},
+                    {format!("{old_indent}{}", if open() { " " } else { " " })},
                     if open() {
                         span { style: "color: var(--light-blue)", " {path}" }
                     } else {
@@ -38,17 +65,27 @@ pub fn TreeFolder(lead: String, file: BlogFile, rec: usize, indent: String, icon
                 }
                 if open() {
                     for (i, child) in children.iter().enumerate() {
-                        TreeFolder { 
+                        TreeFolder {
                             lead: "{lead}{path}/",
                             file: child.clone(),
                             rec: rec + 1,
-                            indent: indent.to_owned() + if i >= child.len() - 1 {"╰ "} else {"│ "},
+                            indent: indent.to_owned() + if i >= child.len() - 1 {" ╰"} else {" │"},
+                            old_indent: indent.to_owned() + "  ",
                             icon: icon.clone()
                         }
                     }
                 }
             },
-            BlogFile::File { path } => rsx! { div { "{indent}{icon} {path}" } },
+            BlogFile::File { path } => rsx! { div { " {indent} {icon} {path}" } },
         }
     }
+}
+
+fn last_two(text: String) -> String {
+    let len = text.chars().count();
+    if len < 2 {
+        return text; // Handle empty or single character strings
+    }
+    let new_len = len - 2;
+    text[..new_len].to_string()
 }
