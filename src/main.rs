a20c9aa46f4a002d8a709b9cd9f118c294b46307
rhaskia <rhaskiagamedev@gmail.com>
2024-12-15 20:02:44 +1300

    basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    // axum web server
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+async fn hello_world() -> &'static str {
+    "Hello world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/", get(|| serve_file("site/index.html")))
+        .nest_service("/assets", ServeDir::new("assets"));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(tcp, router).await.unwrap();
+}
+
+async fn serve_file(path: &str) -> impl IntoResponse {
+    Html(std::fs::read_to_string(path).unwrap())
 }
